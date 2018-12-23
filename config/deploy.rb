192fa859bb9d85b33ab0f7a44b09e9a5a968277b
promiseverma <promiseverma5@gmail.com>
2018-12-23 15:01:20 +0530

    deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,3 +41,16 @@ set :pty, true
 
 # append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
